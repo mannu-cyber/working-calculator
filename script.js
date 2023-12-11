5efbc7f8d00691c69134d68a195891d983fe5264
Abhimanyu sahani <sahanimannu844@gmail.com>
2023-12-11 08:49:38 +0530

    changes made in the first main index file

---
+++ script.js
@@ -0,0 +1,16 @@
+ function pressBtn (keyValue){
+    const inpt = document.getElementById("display")
+   inpt.value = inpt.value + keyValue
+
+
+
+ }
+
+ function calculate (){
+    const inpt = document.getElementById("display")
+inpt.value = eval(inpt.value)
+ }
+
+
+
+
